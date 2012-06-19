6b72275322c2f40483a7b613d42311f60c679620
David Garvey <davegarvey@hotmail.com>
2012-06-19 11:37:04 +0100

    Changed List of Voters to HashSet
    Voters should be unique

--- VotingOption.cs
+++ VotingOption.cs
@@ -1,14 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace VoteR
+﻿namespace VoteR
 {
+    using System.Collections.Generic;
+    using System.Linq;
+
     public class VotingOption
     {
+        public VotingOption(string name)
+        {
+            this.Name = name;
+            this.Voters = new HashSet<string>();
+        }
+
+        /// <summary>
+        /// Voting option name.
+        /// </summary>
         public string Name { get; set; }
         
+        /// <summary>
+        /// Gets the number of votes for this option.
+        /// </summary>
         public int Votes 
         {
             get
@@ -18,26 +28,29 @@ namespace VoteR
         }
 
         /// <summary>
-        /// List of connection ids that have voted for this option.
+        /// HashSet of connection ids that have voted for this option.
         /// </summary>
-        private List<string> Voters { get; set; }
-
-        public VotingOption(string name)
-        {
-            this.Name = name;
-            this.Voters = new List<string>();
-        }
+        private HashSet<string> Voters { get; set; }
 
         public bool UserHasVoted(string cid)
         {
             return Voters.Contains(cid);
         }
 
+        /// <summary>
+        /// Removes a votes from the option.
+        /// </summary>
+        /// <param name="cid">Id of the voter.</param>
+        /// <returns>True if the voter was removed, otherwise false.</returns>
         public bool RemoveVoter(string cid)
         {
             return Voters.Remove(cid);
         }
 
+        /// <summary>
+        /// Adds a voter to the option.
+        /// </summary>
+        /// <param name="cid">Id of the voter.</param>
         public void AddVoter(string cid)
         {
             Voters.Add(cid);
